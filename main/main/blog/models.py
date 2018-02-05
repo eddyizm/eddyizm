810077254f81031fd8137dece4e6e908a37895cd
eddyizm <eddyizm@gmail.com>
2018-02-04 18:12:24 -0800

    Moved blog

---
+++ main/main/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+ 
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.email)
+ 
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    date = models.DateTimeField()
+    author = models.ForeignKey(Author)
+    body = models.TextField()
+ 
+    def __str__(self):
+        return "%s (%s)" % (self.title, self.author.name)
