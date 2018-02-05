c9e461589a75f73135d73ffecae823119fc2e7e9
eddyizm <eddyizm@gmail.com>
2018-02-04 18:59:12 -0800

    moved blog up a directory. This is not going as planned.

--- main/main/urls.py
+++ main/main/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from . import blog
 
 urlpatterns = [
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    #path('blog/', blog.views.index() , name='index'),
+
 ]
