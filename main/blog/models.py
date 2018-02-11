2ad29ec5622fa757bc6e3699f168cdf68361cd63
eddyizm <eddyizm@gmail.com>
2018-02-10 21:38:41 -0800

    Added new blog and tag models

--- main/blog/models.py
+++ main/blog/models.py
@@ -7,12 +7,22 @@ class Author(models.Model):
  
     def __str__(self):
         return "%s (%s)" % (self.name, self.email)
- 
-class Post(models.Model):
-    title = models.CharField(max_length=64)
-    date = models.DateTimeField()
-    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING,)
-    body = models.TextField()
- 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True, default='')
+
     def __str__(self):
-        return "%s (%s)" % (self.title, self.author.name)
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    body = models.CharField(max_length=20000)
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING,)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+        #return "%s (%s)" % (self.title, self.author.name)
+
