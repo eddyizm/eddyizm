f1be9aae716d7a98d5ad64a05d910dd68f7c6eb4
eddyizm <eddyizm@gmail.com>
2018-02-19 12:16:35 -0800

    Updated main landing url and added about content

--- main/blog/urls.py
+++ main/blog/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.detail import DetailView
 from blog import views
 
 urlpatterns = [
-    url(r'^$', views.blog, name='blog'),
+    url(r'^$', views.blog_posts, name='blog'),
     url(r'^blog/(?P<id>[0-9]+)/', views.post_detail, name='post_detail'),   
     url(r'^blog/', views.blog_posts , name='blog_posts'),
     url(r'^podcasts/', views.podcasts, name='podcasts'),
