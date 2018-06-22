459ac7708d68849ecd74fb1e388d1adc516dbec4
Eduardo Cervantes <eddyizm@gmail.com>
2018-06-22 11:24:19 -0700

    added insert daily q method

--- main/blog/quotes.py
+++ main/blog/quotes.py
@@ -1,11 +1,15 @@
 import sqlite3
 import json
+from datetime import datetime as d
 # import os.path
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # db_path = os.path.join(BASE_DIR, "PupilPremiumTable.db")
-# return random quote
+
+# queries
+insertQuoteH = 'INSERT INTO QuoteHistory VALUES (?, ?)'
 sqlite_file = 'main/quotes_app.sqlite3'
 
+# return random quote
 def get_random_q():
   conn = sqlite3.connect(sqlite_file)
   conn.row_factory = sqlite3.Row 
@@ -14,10 +18,19 @@ def get_random_q():
   conn.close()
   return json.dumps( [dict(ix) for ix in rows] )   
 #return json.dumps(dict(c.fetchall()))
+
+def insert_daily_q():
+  currDate = d.now().strftime("%Y-%m-%d")
+  conn = sqlite3.connect(sqlite_file)
+  conn.row_factory = sqlite3.Row 
+  c = conn.cursor()
+  rows = c.execute('''select quote, ID from randomQview''').fetchall()
+  for i in rows:
+     c.execute(insertQuoteH, (currDate, i[1]))
+     conn.commit()
+  conn.close()   
+  #print (rows[1])
   
-#data = c.fetchall()
 # conn.commit()
-#print (json.dumps(c.fetchone()))   
-# 
-# 
-#print (get_random_q())
+
+#insert_daily_q()
