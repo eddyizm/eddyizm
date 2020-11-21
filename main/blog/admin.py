81e2dc8c68d45ac6e98245c4b62d786e4620d16e
eddyizm <eddyizm@gmail.com>
2020-11-21 14:22:35 -0800

    added model to admin

--- main/blog/admin.py
+++ main/blog/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django import forms
-from blog.models import BlogPost, Author, Category
+from blog.models import BlogPost, Author, Category, MusicTrack
 from django.forms import TextInput, Textarea
 from django.db import models
 
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Category)
+admin.site.register(MusicTrack)
+
 # Added to get text area in admin area for blog post.         
 class YourModelAdmin(admin.ModelAdmin):
         formfield_overrides = {
