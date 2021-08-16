0bb73a3a3d2297ee3521dc0f9edac8751c9948a0
eddyizm <eddyizm@gmail.com>
2021-08-16 10:46:56 -0700

    updated urls for prod

--- main/main/urls.py
+++ main/main/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls, name='admin-site'),
     url(r'^', include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG: 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
